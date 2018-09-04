7b658545f237218997d7eab4baad959780d92b25
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-04 14:33:39 -0400

    Changed text in the message bar to the defaultfor when page launches. Also minor improvements to HTML and CSS.

---
+++ main.js
@@ -0,0 +1,13 @@
+//onLoad, prompt player 1 to select a tile
+//select the 'messagebar' element and change the text to a stored variable that contains a string
+
+const MessageBar = () =>{
+    let message = ''
+    //if nothing running, display "Player One's Turn!"
+    $('.messagebar p').text('Player One, Your Turn!')
+}
+
+MessageBar()
+
+
+
