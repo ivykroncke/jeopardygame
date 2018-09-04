738bb099c95b45f2d4c7e120ac4c46d5a1bfbeac
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-04 16:21:37 -0400

    Click event on one of the squares changes the message in the menu bar. Also have the content clicked rendering in the console.

--- main.js
+++ main.js
@@ -2,30 +2,35 @@
 //select the 'messagebar' element and change the text to a stored variable that contains a string
 $(() => {
 
-const catOneQuestions = [
+    //maybe an object is the best way to store these?
+const janswers = [
+    {question: 'This cat loves lasagne', answer: 'who is garfield?'},
+    {question: 'This dog saves little boys out of wells', answer: 'who is lassie?'},
+    {question: 'All dogs go to this place', answer: 'what is heaven?'}
+];
 
-]
-
-
+let userClick = false;
 const MessageBar = () => {
-    //if player just clicked a square
-    
+    if (userClick === true){
+        $('.messagebar p').text('You clicked on an answer!')
+    } else
     //find that corresponding value in the array 
-
     //and display it in the message bar
-
     //else
-    //if nothing running, display "Player One's Turn!"
     $('.messagebar p').text('Player One, Your Turn!')
-}
+} 
 MessageBar()
 
-const showAnswer = () => {
+// $( "#dataTable tbody" ).on( "click", "tr", function() {
+//     console.log( $( this ).text() );
+//   });
+
+$('.answerbox').on('click', 'p', function() {
+    console.log( $( this ).text())
+    userClick = true
     MessageBar()
-}
+})
 
 
-//event listener for board
-$('.answerbox').on('click', showAnswer)
-
+// STOP DELETING THIS!!!! ITS FOR YOUR ENTIRE JS FILE!
 })
