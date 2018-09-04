64917bb3db4a5a75ffb249e2c081348ed7a9fc5d
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-04 15:12:11 -0400

    Added click event for squares and pseudo code for displaying them in the message bar

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 //onLoad, prompt player 1 to select a tile
 //select the 'messagebar' element and change the text to a stored variable that contains a string
+$(() => {
 
-const MessageBar = () =>{
-    let message = ''
+const catOneQuestions = [
+
+]
+
+
+const MessageBar = () => {
+    //if player just clicked a square
+    
+    //find that corresponding value in the array 
+
+    //and display it in the message bar
+
+    //else
     //if nothing running, display "Player One's Turn!"
     $('.messagebar p').text('Player One, Your Turn!')
 }
-
 MessageBar()
 
+const showAnswer = () => {
+    MessageBar()
+}
 
 
+//event listener for board
+$('.answerbox').on('click', showAnswer)
+
+})
