e94daf48721c8cb91e4a99605ea7b5016811ac39
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-04 22:06:55 -0400

    Questions and answers now appearing in the window. Need to come up with a way to randomize the questions.

--- main.js
+++ main.js
@@ -2,55 +2,62 @@
 //select the 'messagebar' element and change the text to a stored variable that contains a string
 $(() => {
 
+    let selectedAnswer = '' // holds the value of the number clicked
+    const allAnswers = [
+        //Category 1
+        { 'id': 'cat1-1', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
+        { 'id': 'cat2-1', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
+        { 'id': 'cat3-1', 'question': 'All dogs go to this place', 'answer': 'what is heaven?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
+    //Category 2
+        { 'id': 'cat1-2', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
+        { 'id': 'cat2-2', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
+        { 'id': 'cat3-2', 'question': 'All dogs go to this place', 'answer': 'what is heaven?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
+    //Category 3
+    { 'id': 'cat1-3', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
+        { 'id': 'cat2-3', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
+        { 'id': 'cat3-3', 'question': 'All dogs go to this place', 'answer': 'what is heaven?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'}
+    ]
 
-let selectedAnswer = '' // holds the value of the number clicked
-const allAnswers = [
+    //Find the question in the array based on user click
+    let questionDisplay
+    let correctAnswer
+    const findQuestion = () => {
+        let matchIndex
+        for (let i = 0; i < allAnswers.length; i++) {
+            if (allAnswers[i].id === selectedAnswer) {
+                matchIndex = [i]
+                console.log('this is the match index: ' + matchIndex)
+            }
+        }
+        questionDisplay = allAnswers[matchIndex].question
+        correctAnswer = allAnswers[matchIndex].answer
+        incorrectAnswer1 = allAnswers[matchIndex].incorrectAnswer1
+        incorrectAnswer2 = allAnswers[matchIndex].incorrectAnswer2
+        incorrectAnswer3 = allAnswers[matchIndex].incorrectAnswer3
+        messageBar()
+    }
 
-    {'id': 'cat1-1', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?'},
-    {'id': 'cat2-1', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?'},
-    {'id': 'cat3-1', 'question': 'All dogs go to this place', 'answer': 'what is heaven?'}
-
-]
-
-let questionDisplay
-const findQuestion = () => {
-    // console.log(allAnswers[0].id)
-    // console.log(selectedAnswer)
-    //loop through the length of the array allAnswers holding objects 
-    let matchIndex   
-    for(let i = 0; i < allAnswers.length; i++){
-        if(allAnswers[i].id === selectedAnswer){
-            matchIndex = [i]
-            console.log('this is the match index: '+ matchIndex)
+    //Message Bar Change 
+    let userClick = false;
+    const messageBar = () => {
+        if (userClick === true) {
+            $('.messagebar p').text(questionDisplay)
+            $('.messagebar p').append(` A. ${correctAnswer} B. ${incorrectAnswer1} C. ${incorrectAnswer2} D. ${incorrectAnswer3}`)
+            $('.messagebar p').innerHTML('Adding some choices here')
+        } else {
+            $('.messagebar p').text('Player One, Your Turn!')
         }
     }
-     //set the questionDisplay variable equal to the match variable
-    questionDisplay = allAnswers[matchIndex].question
-    messageBar()    
-    }
+    messageBar()
 
-    //store the index of that key
-    //use the key to find the answer pair
-    //save that to display in the messageBar function}
-
-let userClick = false;
-const messageBar = () => {
-    if (userClick === true){ 
-        $('.messagebar p').text(questionDisplay)
-    } else {
-    $('.messagebar p').text('Player One, Your Turn!')
-    }
-} 
-messageBar() //run the function
-
-//click event - calls the messageBar function
-$('.answerbox').on('click', function($event) {
-    selectedAnswer = $event.target.id
-    userClick = true //changes the status for a diff result in messageBar
-    findQuestion()
-    messageBar() //runs the function
-})
+    //click event - calls the messageBar function
+    $('.answerbox').on('click', function ($event) {
+        selectedAnswer = $event.target.id
+        userClick = true //changes the status for a diff result in messageBar
+        findQuestion()
+        messageBar() //runs the function
+    })
 
 
-// STOP DELETING THESE BELOW!!!! ITS FOR YOUR ENTIRE JS FILE!
+    // DO NOT DELETE THESE BELOW!
 })
