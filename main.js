a925095defc0583c295744be41e9bdb712507088
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-04 22:37:01 -0400

    Added some pseudocode and variables/functions I think I will need for the next steps while in a groove.

--- main.js
+++ main.js
@@ -5,18 +5,21 @@ $(() => {
     let selectedAnswer = '' // holds the value of the number clicked
     const allAnswers = [
         //Category 1
-        { 'id': 'cat1-1', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
-        { 'id': 'cat2-1', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
-        { 'id': 'cat3-1', 'question': 'All dogs go to this place', 'answer': 'what is heaven?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
-    //Category 2
-        { 'id': 'cat1-2', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
-        { 'id': 'cat2-2', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
-        { 'id': 'cat3-2', 'question': 'All dogs go to this place', 'answer': 'what is heaven?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
-    //Category 3
-    { 'id': 'cat1-3', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
-        { 'id': 'cat2-3', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'},
-        { 'id': 'cat3-3', 'question': 'All dogs go to this place', 'answer': 'what is heaven?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong'}
+        { 'id': 'cat1-1', 'points': '$500', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong' },
+        { 'id': 'cat2-1', 'points': '$500', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong' },
+        { 'id': 'cat3-1', 'points': '$500', 'question': 'All dogs go to this place', 'answer': 'what is heaven?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong' },
+        //Category 2
+        { 'id': 'cat1-2', 'points': '$750', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong' },
+        { 'id': 'cat2-2', 'points': '$750', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong' },
+        { 'id': 'cat3-2', 'points': '$750', 'question': 'All dogs go to this place', 'answer': 'what is heaven?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong' },
+        //Category 3
+        { 'id': 'cat1-3', 'points': '$1000', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong' },
+        { 'id': 'cat2-3', 'points': '$1000', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong' },
+        { 'id': 'cat3-3', 'points': '$1000', 'question': 'All dogs go to this place', 'answer': 'what is heaven?', 'incorrectAnswer1': 'wrong', 'incorrectAnswer2': 'wrong', 'incorrectAnswer3': 'wrong' }
     ]
+
+    let playerOneTotal = 0
+    let PlayerTwoTotal = 0
 
     //Find the question in the array based on user click
     let questionDisplay
@@ -26,7 +29,6 @@ $(() => {
         for (let i = 0; i < allAnswers.length; i++) {
             if (allAnswers[i].id === selectedAnswer) {
                 matchIndex = [i]
-                console.log('this is the match index: ' + matchIndex)
             }
         }
         questionDisplay = allAnswers[matchIndex].question
@@ -42,13 +44,33 @@ $(() => {
     const messageBar = () => {
         if (userClick === true) {
             $('.messagebar p').text(questionDisplay)
-            $('.messagebar p').append(` A. ${correctAnswer} B. ${incorrectAnswer1} C. ${incorrectAnswer2} D. ${incorrectAnswer3}`)
-            $('.messagebar p').innerHTML('Adding some choices here')
-        } else {
-            $('.messagebar p').text('Player One, Your Turn!')
+            $('.messagebar p').append(` A. ${correctAnswer} B. ${incorrectAnswer1} C. ${incorrectAnswer2} D. ${incorrectAnswer3}`) //need to randomize these...
+            $(`.messagebar p`).on('click', function ($event) {
+                console.log($event.target)
+                if ($event.target !== `${correctAnswer}`) {
+                    console.log('wrong answer') //and trigger wrong answer response
+                } else console.log('right answer') //and trigger right answer response
+            })
+        } else {$('.messagebar p').text('Player 1, Please Select a Square to Begin!')
         }
     }
     messageBar()
+
+    const incorrectUserResponse = () => {
+        $('.messagebar p').text('Your answer was incorrect!')
+            // !Current Player turn
+            //start the squareAnswered function for the question's corresponding square
+    }
+
+    const correctUserResponse = () => {
+        $('.messagebar p').text('Correct!')
+            // Add amount for question to current player
+            //start the squareAnswered function for the question's corresponding square
+    }
+
+    const squareAnswered = () => {
+        //if the square has been answered, change the state of the square
+    }
 
     //click event - calls the messageBar function
     $('.answerbox').on('click', function ($event) {
