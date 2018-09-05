2da2701a34f65bdc665a97627c7c2c7218c60f63
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-04 21:31:23 -0400

    Question displays in the message bar according to which square is clicked. Needs more organization, but logic in place.

--- main.js
+++ main.js
@@ -2,34 +2,30 @@
 //select the 'messagebar' element and change the text to a stored variable that contains a string
 $(() => {
 
+
 let selectedAnswer = '' // holds the value of the number clicked
 const allAnswers = [
 
     {'id': 'cat1-1', 'question': 'This cat loves lasagna', 'answer': 'who is garfield?'},
-
     {'id': 'cat2-1', 'question': 'This dog saves little boys out of wells', 'answer': 'who is lassie?'},
-
     {'id': 'cat3-1', 'question': 'All dogs go to this place', 'answer': 'what is heaven?'}
 
 ]
 
-let questionDisplay = ''
+let questionDisplay
 const findQuestion = () => {
-    console.log(allAnswers[0].id)
-    console.log(selectedAnswer)
+    // console.log(allAnswers[0].id)
+    // console.log(selectedAnswer)
     //loop through the length of the array allAnswers holding objects 
-    let match = ''   
+    let matchIndex   
     for(let i = 0; i < allAnswers.length; i++){
-        //if the key id matches the value in the variable selected answer
-        if(allAnswers[i].id === 'cat1-1'){
-        //store that value in the match variable
-
-        } 
-        console.log(match)
-        questionDisplay = match
-        //set the questionDisplay variable equal to the match variable
-        console.log(questionDisplay)
+        if(allAnswers[i].id === selectedAnswer){
+            matchIndex = [i]
+            console.log('this is the match index: '+ matchIndex)
+        }
     }
+     //set the questionDisplay variable equal to the match variable
+    questionDisplay = allAnswers[matchIndex].question
     messageBar()    
     }
 
@@ -40,7 +36,7 @@ const findQuestion = () => {
 let userClick = false;
 const messageBar = () => {
     if (userClick === true){ 
-        $('.messagebar p').text('test text')
+        $('.messagebar p').text(questionDisplay)
     } else {
     $('.messagebar p').text('Player One, Your Turn!')
     }
@@ -53,7 +49,6 @@ $('.answerbox').on('click', function($event) {
     userClick = true //changes the status for a diff result in messageBar
     findQuestion()
     messageBar() //runs the function
-
 })
 
 
